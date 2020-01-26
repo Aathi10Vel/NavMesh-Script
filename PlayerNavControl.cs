538f7d036ac2baf81e097dd2d026b0ea61b604a9
Aathi <37983032+Aathi10Vel@users.noreply.github.com>
2020-01-26 17:41:17 +0530

    Add files via upload

---
+++ PlayerNavControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavControl : MonoBehaviour
+{
+    public ThirdPersonControl crypto_NavCharacter;
+    public Camera navCamera;
+    public NavMeshAgent crypto_NavAgent;
+    Ray crypto_NavRay;
+    RaycastHit crypto_NavHitRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        crypto_NavAgent.updateRotation = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMovePlayer();
+    }
+
+    public void NavMovePlayer()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            crypto_NavRay = navCamera.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(crypto_NavRay,out crypto_NavHitRay))
+            {
+                crypto_NavAgent.SetDestination(crypto_NavHitRay.point);
+            }
+        }
+
+        if (crypto_NavAgent.remainingDistance > crypto_NavAgent.stoppingDistance)
+        {
+            crypto_NavCharacter.Crypto_Move(crypto_NavAgent.desiredVelocity);
+        }
+        else
+        {
+            crypto_NavCharacter.Crypto_Move(Vector3.zero);
+        }
+    }
+}
